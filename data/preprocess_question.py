78cbc0b854cdbeed5a97c774d7cdb722a8634771
JasperGuo <jasperguo2015@gmail.com>
2017-07-19 22:56:03 +0800

    add training data

---
+++ data/preprocess_question.py
@@ -0,0 +1,129 @@
+# coding=utf8
+import os
+import re
+import json
+from bson import json_util
+from preprocess_table import process_value
+
+COLUMN_PATTERN = re.compile("^col__(.*?)__\((\d+), (\d+)\)$")
+CELL_PATTERN = re.compile("^cell__(.*?)__\((\d+), (\d+)\)$")
+TABLE_PATTERN = re.compile("^TABLE__(.*)$")
+
+
+def read_file(file):
+    content = list()
+    with open(file) as f:
+        for line in f:
+            content.append(json.loads(line.strip()))
+    return content
+
+
+def get_table_map_id(table_loc):
+    return '/'.join(table_loc).replace(".csv", ".table")
+
+
+def process_labeled_sequence(sequence):
+    labeled_sequence = list()
+    for s in sequence:
+        begin = s[0]
+        end = s[1]
+        assert begin <= end
+        label = s[2].strip()
+        i = begin
+        while i <= end:
+            labeled_sequence.append(label)
+            i += 1
+    return labeled_sequence
+
+
+def get_labeled_sequence_index(sequence, table):
+    """
+    PAT: 0
+    LIT: 1
+    TAB: 2
+    COLUMN_NAME:
+    COLUMNS:
+    :param sequence:
+    :param table:
+    :return:
+    """
+    columns = table["columns"]
+    label_index = list()
+    for label in sequence:
+        if label.lower() == "pat":
+            label_index.append(0)
+        else:
+            match = COLUMN_PATTERN.search(label)
+            if match:
+                # COL
+                value = process_value(match.group(1))
+                column_idx = match.group(3)
+                actual_idx = 2 + 1 + int(column_idx)
+                label_index.append(actual_idx)
+            else:
+                # CELL
+                cell_match = CELL_PATTERN.search(label)
+                if cell_match:
+                    value = process_value(cell_match.group(1))
+                    column_idx = int(cell_match.group(3))
+                    offset = 0
+                    for i in range(column_idx):
+                        offset += len(columns[i])
+                    offset += (2 + 1 + len(table["column_name"]))
+                    index = columns[column_idx].index(value)
+                    actual_idx = offset + index
+                    label_index.append(actual_idx)
+                else:
+                    # Table
+                    table_match = TABLE_PATTERN.search(label)
+                    if table_match:
+                        actual_idx = 2
+                        label_index.append(actual_idx)
+                    else:
+                        actual_idx = 1
+                        label_index.append(actual_idx)
+    return label_index
+
+
+def process_question(question, table_dict):
+    tokenized_sentence = question["tokenized_sentence"].strip().split("|")
+    table_map_id = get_table_map_id(question["table_loc"])
+    labeled_sequence = process_labeled_sequence(question["bound_sequence"])
+    question_id = question["_id"]
+    table = table_dict[table_map_id]
+    label_sequence_index = get_labeled_sequence_index(labeled_sequence, table)
+
+    return {
+        "tokenized_sentence": tokenized_sentence,
+        "table_map_id": table_map_id,
+        "labeled_sequence": labeled_sequence,
+        "question_id": question_id,
+        "label_sequence_index": label_sequence_index
+    }
+
+
+def main(file, table_file):
+    questions = read_file(file)
+    tables = read_file(table_file)
+
+    table_dict = dict()
+    for t in tables:
+        table_dict[t["map_id"]] = t
+
+    result = list()
+    for q in questions:
+        try:
+            result.append(json.dumps(process_question(q, table_dict), default=json_util))
+        except Exception as e:
+            print(e)
+            continue
+
+    file_base_name = os.path.basename(file)
+    dirname = os.path.dirname(file)
+    with open(os.path.join(dirname, "preprocessed_" + file_base_name), "w") as f:
+        f.write('\n'.join(result))
+    print(len(result))
+
+
+if __name__ == "__main__":
+    main("training_questions.txt", "preprocessed_training_tables.txt")
---
+++ data/preprocess_table.py
@@ -0,0 +1,126 @@
+# coding=utf8
+
+import os
+import re
+import json
+import argparse
+from bson import json_util
+
+NUMBER_PATTERN1 = re.compile("^\d*\.?\d*$")
+NUMBER_PATTERN2 = re.compile("^([0-9,]+)$")
+YEAR_PATTERN = re.compile("^[12][0-9]{3}$")
+MONTH_PATTERN = re.compile("(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec|january|february|march|april|may|june|july|august|september|october|november|december)")
+
+
+def read_file(file):
+    tables = list()
+    with open(file) as f:
+        for line in f:
+            tables.append(json.loads(line.strip()))
+    return tables
+
+
+def process_value(value):
+    value = value.strip().lower()
+    if len(value) == 0:
+        value = "-"
+    if "%" in value:
+        value = value.replace("%", " percentage ")
+    if "#" in value:
+        value = value.replace("#", " number ")
+    # Replace multiple spaces with single space
+    value = re.sub(' +', ' ', value)
+    return value
+
+
+def group_table_by_column(rows):
+    assert len(rows) > 0
+    column_num = len(rows[0])
+    columns = [[] for i in range(column_num)]
+    for r in rows:
+        for i in range(column_num):
+            value = process_value(r[i])
+            columns[i].append(value)
+    return columns
+
+
+def check_value_type(value):
+    if YEAR_PATTERN.search(value):
+        return "DATE"
+    elif MONTH_PATTERN.search(value):
+        return "DATE"
+    elif NUMBER_PATTERN1.search(value):
+        return "NUMERIC"
+    elif NUMBER_PATTERN2.search(value):
+        return "NUMERIC"
+    return "STRING"
+
+
+def check_column_type(column_value_type):
+    data_type = dict()
+    for t in column_value_type:
+        if t not in data_type:
+            data_type[t] = 0
+        data_type[t] += 1
+
+    max_type = ("", -1)
+    for t, count in data_type.items():
+        if count > max_type[1]:
+            max_type = (t, count)
+
+    return max_type[0]
+
+
+def process_table(table):
+    columns = group_table_by_column(table["rows"])
+
+    clean_columns = list()
+    column_name = list()
+    for c in columns:
+        column_name.append(c.pop(0))
+        clean_columns.append(list(set(c)))
+
+    column_type = list()
+    for c in clean_columns:
+        data_type = list()
+        for value in c:
+            data_type.append(check_value_type(value))
+        column_type.append(check_column_type(data_type))
+    """
+    print(table["_id"])
+    print("column_name: ", column_name)
+    print("column_type: ", column_type)
+    print("columns: ", clean_columns)
+    """
+
+    table_info = {
+        "columns": clean_columns,
+        "column_name": column_name,
+        "column_type": column_type,
+        "table_name": process_value(table["title"]),
+        "map_id": table["map_id"],
+        "_id": table["_id"]
+    }
+
+    return json.dumps(table_info, default=json_util.default)
+
+
+def main(file):
+    tables = read_file(file)
+    result = list()
+    for table in tables:
+        try:
+            result.append(process_table(table))
+        except Exception as e:
+            print(e)
+    file_base_name = os.path.basename(file)
+    dirname = os.path.dirname(file)
+    with open(os.path.join(dirname, "preprocessed_" + file_base_name), "w") as f:
+        f.write('\n'.join(result))
+    print(len(result))
+
+if __name__ == "__main__":
+    # arg_parser = argparse.ArgumentParser("Path of tables file")
+    # arg_parser.add_argument("--file", help="path")
+    # args = arg_parser.parse_args()
+    main("training_tables.txt")
